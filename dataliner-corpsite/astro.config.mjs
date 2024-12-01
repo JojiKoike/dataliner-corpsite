9032a4c8a28ab65f7e4c22eedd2cdb65b406ddea
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-01 21:39:35 +0900

    ✨ feat(dataliner-corpsite Deploy): デプロイ設定
    Vercel用デプロイ設定。VercelのAnalytics機能もついでに

--- dataliner-corpsite/astro.config.mjs
+++ dataliner-corpsite/astro.config.mjs
@@ -5,7 +5,16 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    }
+  }),
 });
