42c6f7bcc414202a3b3c6dbef6237db6c25f8bd7
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-05 07:19:28 +0900

    ✨ feat(dataliner-corpsite LandingPage): CMS
    ランディングページのコンテンツをNewt管理にした

--- dataliner-corpsite/src/lib/newt.ts
+++ dataliner-corpsite/src/lib/newt.ts
@@ -6,6 +6,28 @@ export interface Article {
   body: string;
 }
 
+export interface Page {
+  pageName: string;
+  slug: string;
+  meta: Meta;
+  sections: Section[];
+}
+
+interface Meta {
+  title: string;
+  description: string;
+}
+
+interface Section {
+  type: string;
+  data: Data;
+}
+
+interface Data {
+  titleCopy: string;
+  text: string;
+}
+
 export const newtClient = createClient({
   spaceUid: import.meta.env.NEWT_SPACE_UID,
   token: import.meta.env.NEWT_CDN_API_TOKEN,
