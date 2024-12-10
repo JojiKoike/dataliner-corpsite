207d30c70a8e823021fa6b6711782f1ad009cb8c
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-11 01:12:24 +0900

    🦄 refactor(dataliner-corpsite Blog): Refactor
    型定義の実装を分割

--- dataliner-corpsite/src/lib/newt.ts
+++ dataliner-corpsite/src/lib/newt.ts
@@ -1,70 +1,7 @@
 import { createClient } from 'newt-client-js';
 
-export interface Article {
-  title: string;
-  _sys: Sys;
-  abstract: string;
-  slug: string;
-  body: string;
-  category: Category;
-  tags: Tag[];
-  coverImage: ArticleImage;
-  meta: ArticleMeta;
-}
-
-export interface Sys {
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface Category {
-  name: string;
-  slug: string;
-}
-
-export interface Tag {
-  name: string;
-  slug: string;
-}
-
-export interface ArticleImage {
-  src: string;
-  width: number;
-  height: number;
-  title: string;
-  altText: string;
-}
-
-export interface ArticleMeta {
-  title: string;
-  description: string;
-  ogImage: ArticleImage;
-}
-
-export interface Page {
-  pageName: string;
-  slug: string;
-  meta: Meta;
-  sections: Section[];
-}
-
-interface Meta {
-  title: string;
-  description: string;
-}
-
-interface Section {
-  type: string;
-  data: Data;
-}
-
-interface Data {
-  titleCopy: string;
-  text: string;
-}
-
 export const newtClient = createClient({
   spaceUid: import.meta.env.NEWT_SPACE_UID,
   token: import.meta.env.NEWT_CDN_API_TOKEN,
-  apiType: 'cdn',
+  apiType: import.meta.env.NEWT_API_TYPE,
 });
---
+++ dataliner-corpsite/src/types/blog/article.ts
@@ -0,0 +1,18 @@
+import type { Content, Media } from 'newt-client-js';
+import type { Category } from './category';
+import type { Tag } from './tag';
+
+export interface Article extends Content {
+  title: string;
+  abstract: string;
+  slug: string;
+  meta?: {
+    title?: string;
+    description?: string;
+    ogImage?: Media;
+  };
+  body: string;
+  coverImage?: Media;
+  category: Category;
+  tags: Tag[];
+}
