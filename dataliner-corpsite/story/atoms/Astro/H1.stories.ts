15dec59a5f920b5480102cafd28fde3edf376503
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-21 21:32:40 +0900

    ✨ feat(dataliner-corpsite 共通コンポーネント): 新規追加
    見出しレベル１のAstroコンポーネント追加

---
+++ dataliner-corpsite/story/atoms/Astro/H1.stories.ts
@@ -0,0 +1,12 @@
+import H1 from '../../../src/components/atoms/H1.astro';
+
+export default {
+  component: H1,
+};
+
+export const Primary = {
+  args: {
+    children: '見出し1',
+    className: '',
+  },
+};
