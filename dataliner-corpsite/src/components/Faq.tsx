f371a2a582ac866da647b79d61a43db94cea74d9
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-21 20:45:46 +0900

    🦄 refactor(dataliner-corpsite Refactor): リファクタリング
    h2を共通コンポーネントに置き換え

--- dataliner-corpsite/src/components/Faq.tsx
+++ dataliner-corpsite/src/components/Faq.tsx
@@ -1,12 +1,13 @@
 import type { Faq } from '../types/page/page';
+import H2 from './atoms/H2';
 import FAQItem from './FaqItem';
 
 const FAQ = ({ faq }: { faq: Faq }) => {
   return (
     <section className="lg:px-15 mx-auto mb-16 max-w-screen-lg px-6 sm:mb-24 sm:px-10">
-      <h2 className="mb-7 text-center text-2xl font-bold leading-snug sm:mb-12 sm:text-3xl">
+      <H2 id="landing-page-qa-title" className="text-center">
         よくある質問
-      </h2>
+      </H2>
       <div className="lg:px-15 text-gray-600">
         {faq.data.items.map((item) => (
           <FAQItem key={faq._id} item={item} />
