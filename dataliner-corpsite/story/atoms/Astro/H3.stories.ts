474ddf4954798b1c9a34088a1eb858d4fb726ebb
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-22 07:44:24 +0900

    ✨ feat(dataliner-corpsite Component): 新規追加
    第３レベルの見出しのコンポーネント作成

---
+++ dataliner-corpsite/story/atoms/Astro/H3.stories.ts
@@ -0,0 +1,12 @@
+import H3 from '../../../src/components/atoms/H3.astro';
+
+export default {
+  component: H3,
+};
+
+export const Primary = {
+  args: {
+    children: '見出し３',
+    className: '',
+  },
+};
