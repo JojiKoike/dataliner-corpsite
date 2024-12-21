b5a19225a9306cd05af5de39f4825246907366be
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-21 12:14:36 +0900

    ✨ feat(dataliner-corpsite AstroBook): 導入
    StoryBookのAstro対応版、AstroBookの導入

--- dataliner-corpsite/astro.config.mjs
+++ dataliner-corpsite/astro.config.mjs
@@ -1,16 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
-
+import astrobook from 'astrobook';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), astrobook({ subpath: '/astrobook', directory: 'story' })],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
