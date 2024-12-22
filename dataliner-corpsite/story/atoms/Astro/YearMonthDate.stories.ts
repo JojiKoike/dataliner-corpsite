4a7218c840f751688ece29b830292d0b90f1715d
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-22 14:57:34 +0900

    ✨ feat(dateliner-corpsite AtomicDesign): 新規コンポーネント追加
    Atomに年月日を表示するコンポーネントの追加

---
+++ dataliner-corpsite/story/atoms/Astro/YearMonthDate.stories.ts
@@ -0,0 +1,13 @@
+import YearMonthDate from '../../../src/components/atoms/YearMonthDate.astro';
+
+export default {
+  component: YearMonthDate,
+};
+
+export const Primary = {
+  args: {
+    id: 'year-month-date',
+    datetime: '2023-01-01T00:00:00.000Z',
+    className: '',
+  },
+};
