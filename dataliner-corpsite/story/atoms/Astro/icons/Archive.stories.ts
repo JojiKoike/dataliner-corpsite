6fc4c0f5b95bfaf25d91f4936ca1bc8ca5093a37
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-28 16:52:44 +0900

    ✨ feat(dataliner-corpsite AtomicDesign): Atomsにコンポーネント追加
    アーカイブアイコンを表示するコンポーネントをAtomsに追加

---
+++ dataliner-corpsite/story/atoms/Astro/icons/Archive.stories.ts
@@ -0,0 +1,18 @@
+import Archive from '../../../../src/components/atoms/icons/Archive.astro';
+
+export default {
+  component: Archive,
+};
+
+export const Outline = {
+  args: {
+    className: '',
+  },
+};
+
+export const Filled = {
+  args: {
+    className: '',
+    isFilled: true,
+  },
+};
