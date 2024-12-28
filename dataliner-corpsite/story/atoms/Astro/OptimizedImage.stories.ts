e3aa50510ef32ce2cbe2540230b77025a38f1ccd
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-28 11:46:53 +0900

    ✨ feat(dataliner-corpsite AtomicDesign): Atomsコンポーネント追加
    webp形式に最適化する画像コンポーネントをAtomsに追加

---
+++ dataliner-corpsite/story/atoms/Astro/OptimizedImage.stories.ts
@@ -0,0 +1,15 @@
+import OptimizedImage from '../../../src/components/atoms/OptimizedIImage.astro';
+
+export default {
+  component: OptimizedImage,
+};
+
+export const Primary = {
+  args: {
+    id: 'optimized-image',
+    src: 'https://storage.googleapis.com/newt-files/c1329f7d-ff39-448a-b023-dbee3c2a409f%2Fhannah-busing-7UWVcLtrhNk-unsplash.jpg',
+    width: 600,
+    height: 400,
+    className: '',
+  },
+};
