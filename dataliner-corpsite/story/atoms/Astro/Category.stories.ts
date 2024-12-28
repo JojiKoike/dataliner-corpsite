7ed20718b3c3d4b77001334b9ede57d101294001
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-28 11:48:44 +0900

    ✨ feat(dataliner-corpsite AtomicDesign): Atomsコンポーネント追加
    Category表示用Atomsコンポーネント追加

---
+++ dataliner-corpsite/story/atoms/Astro/Category.stories.ts
@@ -0,0 +1,12 @@
+import Category from '../../../src/components/atoms/Category.astro';
+
+export default {
+  component: Category,
+};
+
+export const Primary = {
+  args: {
+    category: 'カテゴリー',
+    className: '',
+  },
+};
