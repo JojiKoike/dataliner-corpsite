0ced75d6861d97d0daab0514e43a2d74aa3b78cf
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-28 15:35:51 +0900

    🔧 build(dataliner-corpsite AtomicDesign): アイコンフォント追加
    iconfyに対応したアイコンフォントコンポーネント集Astro Iconを追加

--- dataliner-corpsite/astro.config.mjs
+++ dataliner-corpsite/astro.config.mjs
@@ -5,9 +5,11 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 import astrobook from 'astrobook';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), astrobook({ subpath: '/astrobook', directory: 'story' })],
+  integrations: [tailwind(), react(), astrobook({ subpath: '/astrobook', directory: 'story' }), icon()],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
