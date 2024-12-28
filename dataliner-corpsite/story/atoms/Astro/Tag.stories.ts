6e28dd996ce7a551f584cad2f21479df7b133592
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-28 11:49:56 +0900

    ✨ feat(dataliner-cospsite AtomicDesign): Atomsコンポーネント追加
    タグ表示用Atomsコンポーネント追加

---
+++ dataliner-corpsite/story/atoms/Astro/Tag.stories.ts
@@ -0,0 +1,12 @@
+import Tag from '../../../src/components/atoms/Tag.astro';
+
+export default {
+  component: Tag,
+};
+
+export const Primary = {
+  args: {
+    tag: 'タグ',
+    className: '',
+  },
+};
