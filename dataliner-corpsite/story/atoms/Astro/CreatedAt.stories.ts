2890df2c1fd381fed89d13969eb8a7ac6defaa32
Joji Koike <5671422+JojiKoike@users.noreply.github.com>
2024-12-28 15:38:36 +0900

    ✨ feat(dataliner-corpsite AtomicDesign): Atomsにコンポーネント追加
    作成日アイコンコンポーネントをAtomsに追加

---
+++ dataliner-corpsite/story/atoms/Astro/CreatedAt.stories.ts
@@ -0,0 +1,11 @@
+import CreatedAt from '../../../src/components/atoms/CreatedAt.astro';
+
+export default {
+  component: CreatedAt,
+};
+
+export const Primary = {
+  args: {
+    className: '',
+  },
+};
